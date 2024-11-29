07c3a101a90c0a573c2f50e58df0dbfb670b6001
Christian Martinez <cristian_123ivan@outlook.com>
2024-11-29 12:53:47 -0600

    ALLOWED HOST

--- crud_usuariosCIMM/settings.py
+++ crud_usuariosCIMM/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-a02au8g-g0c5$%4$b5_+wttk*d2vhizi)*7ji)elpr%1#m*5tj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
